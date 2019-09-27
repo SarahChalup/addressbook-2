79b87980182e914873dc849fa4a075beb93a5464
sarit <sarit@DESKTOP-KSJF0BF>
2019-09-27 16:09:50 -0400

    Commit

---
+++ mvcAddress/mvcAddress/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace mvcAddress.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        instagram,
+        linkedin
+    }
+    public class Book
+    {
+        [Key]
+        public int bookID { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public TypeContact type { get; set; }
+        [Required]
+        public string contact { get; set; }
+
+       
+    }
+}
